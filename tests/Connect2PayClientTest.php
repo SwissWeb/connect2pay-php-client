7f3ee1adeda393c4aba0697dfbe1cb5e1974b98e
heitor dolinski <heitor.dolinski@payxpert.com>
2017-10-26 12:35:21 +0200

    travis CI

--- tests/Connect2PayClientTest.php
+++ tests/Connect2PayClientTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use PHPUnit\Framework\TestCase;
 use PayXpert\Connect2Pay\Connect2PayClient;
 
 /**
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,3 @@
 <?php
 
-require_once basename(__DIR__) . '/../vendor/autoload.php';
 require_once basename(__DIR__) . '/../src/Connect2PayClient.php';
